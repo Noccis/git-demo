55d70b8a219a6b342087ea9fcbf71a4d52575798
Toni Löf <89582846+Noccis@users.noreply.github.com>
2024-12-12 12:38:08 +0100

    created sidebar with info (#4)

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Sidebar = () => {
+  return (
+    <aside>
+      <h3>Git Basics</h3>
+      <ul>
+        <li>Vad är Git?</li>
+        <li>Varför versionhantering?</li>
+        <li>Grundläggande kommandon</li>
+        <li>GitHub och Remote Repos</li>
+      </ul>
+    </aside>
+  );
+};
+
+export default Sidebar;
