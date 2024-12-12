78178fcb11ab58f913839edab961f67eea5205c3
Noccis <toniwincent83@gmail.com>
2024-12-12 12:32:31 +0100

    created sidebar with info

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import './App.css'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
+import Sidebar from './components/Sidebar';
 
 function App() {
 
   return (
     <>
       <Header />
-      <MainContent />
+      <div className='flex'>
+        <MainContent />
+        <Sidebar />
+      </div>
     </>
   )
 }
